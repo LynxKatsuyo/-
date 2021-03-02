fc0b46c0bba384c3dc1ed028a258814fbbf18791
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-03-02 11:54:52 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -77,7 +77,7 @@ And the following:
 SAITAMA_IMG = "https://telegra.ph/file/70ec32568113e5728702b.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
-Saitama is hosted on one of Kaizoku's Servers and doesn't require any donations as of now but \
+
 You can donate to the original writer of the Base code, Paul
 There are two ways of supporting him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
 
