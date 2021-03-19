169478276794fad7286789f17e113234ef809767
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-03-19 15:54:20 +0530

    Update userinfo.py

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -226,7 +226,7 @@ def info(update: Update, context: CallbackContext):
     rep = message.reply_text(
         "<code>Appraising Nyah!!...</code>", parse_mode=ParseMode.HTML)
 
-    text = (f"╒═══「<b> Appraisal results keko!! :</b> 」\n"
+    text = (f"╒═══「<b> Nyah!! :</b> 」\n"
             f"ID: <code>{user.id}</code>\n"
             f"First Name: {html.escape(user.first_name)}")
 
