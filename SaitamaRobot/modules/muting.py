a33669a74ea1a0b68601842a78c38d9301481687
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-03-22 20:27:43 +0530

    Update muting.py

--- SaitamaRobot/modules/muting.py
+++ SaitamaRobot/modules/muting.py
@@ -222,9 +222,9 @@ def temp_mute(update: Update, context: CallbackContext) -> str:
 
 
 __help__ = """
-*Admins only:*
- • `/mute <userhandle>`*:* silences a user. Can also be used as a reply, muting the replied to user.
- • `/tmute <userhandle> x(m/h/d)`*:* mutes a user for x time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
+*Admemes only:*
+ • `/mute <userhandle>`*:*tapes a user's whole face. Can also be used as a reply, muting the replied to user.
+ • `/tmute <userhandle> x(m/h/d)`*:* tapes a user for x time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
  • `/unmute <userhandle>`*:* unmutes a user. Can also be used as a reply, muting the replied to user.
 """
 
