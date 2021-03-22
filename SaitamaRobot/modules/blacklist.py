252a1dc6ec75232ec213cdca0e43be0dd5b56480
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-03-22 20:17:31 +0530

    Update blacklist.py

--- SaitamaRobot/modules/blacklist.py
+++ SaitamaRobot/modules/blacklist.py
@@ -462,9 +462,9 @@ Blacklists are used to stop certain triggers from being said in a group. Any tim
  • `/blacklist`*:* View the current blacklisted words.
 
 Admin only:
- • `/addblacklist <triggers>`*:* Add a trigger to the blacklist. Each line is considered one trigger, so using different lines will allow you to add multiple triggers.
- • `/unblacklist <triggers>`*:* Remove triggers from the blacklist. Same newline logic applies here, so you can remove multiple triggers at once.
- • `/blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>`*:* Action to perform when someone sends blacklisted words.
+ • `/addblacklist <triggers>`*:* Curse a word!!  Each line is considered one trigger, so using different lines will allow you to add multiple triggers.
+ • `/unblacklist <triggers>`*:* Remove words from the curse!! Same newline logic applies here, so you can remove multiple triggers at once.
+ • `/blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>`*:* The curse power you want.. it ranges as the spell i cast are different..... 
 
 """
 BLACKLIST_HANDLER = DisableAbleCommandHandler(
