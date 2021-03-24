a53a019630c47426c1d01c8cd6b0cdf334d20ec1
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-03-24 15:02:08 +0530

    Update cust_filters.py

--- SaitamaRobot/modules/cust_filters.py
+++ SaitamaRobot/modules/cust_filters.py
@@ -566,8 +566,8 @@ def get_exception(excp, filt, chat):
 def addnew_filter(update, chat_id, keyword, text, file_type, file_id, buttons):
     msg = update.effective_message
     totalfilt = sql.get_chat_triggers(chat_id)
-    if len(totalfilt) >= 150:  # Idk why i made this like function....
-        msg.reply_text("This group has reached its max filters limit of 150.")
+    if len(totalfilt) >= 700:  # Idk why i made this like function....
+        msg.reply_text("This group has reached its max filters limit of 700.")
         return False
     else:
         sql.new_add_filter(chat_id, keyword, text, file_type, file_id, buttons)
