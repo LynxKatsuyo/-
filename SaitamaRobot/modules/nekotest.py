a674f2e59741f06dec09528091e96e45689cc2c8
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-04-05 17:17:46 +0530

    Update nekotest.py

--- SaitamaRobot/modules/nekotest.py
+++ SaitamaRobot/modules/nekotest.py
@@ -4,13 +4,13 @@ from SaitamaRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
-
-def neko(update: Update, context: CallbackContext ):
+@run_async 
+async def neko(update: Update, context: CallbackContext ):
     msg = update.effective_message
     user = update.effective_message.message_id
     chat = update.effective_chat
-    nek = await nekos.img("neko")
-    await bot.send_photo(chat_id = chat, photo = nek, reply_to_message_id = user)
+    nek = nekos.img("neko")
+    bot.send_photo(chat_id = chat, photo = nek, reply_to_message_id = user)
    
   
 NEKO_HANDLER = DisableAbleCommandHandler("neko", neko)
