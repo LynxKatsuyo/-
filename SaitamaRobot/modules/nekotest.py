2f2383a3ab029bca0abf0a85381e93ea8217cecc
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-04-05 17:21:02 +0530

    Update nekotest.py

--- SaitamaRobot/modules/nekotest.py
+++ SaitamaRobot/modules/nekotest.py
@@ -4,8 +4,8 @@ from SaitamaRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
-@run_async 
-async def neko(update: Update, context: CallbackContext ):
+
+def neko(update: Update, context: CallbackContext ):
     msg = update.effective_message
     user = update.effective_message.message_id
     chat = update.effective_chat
