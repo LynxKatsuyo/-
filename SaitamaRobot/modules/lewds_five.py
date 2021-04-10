a088e756a8c20c0ed1ee74d1d549ca08b9b47485
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-04-10 16:06:03 +0530

    Add files via upload

---
+++ SaitamaRobot/modules/lewds_five.py
@@ -0,0 +1,250 @@
+import nekos
+from SaitamaRobot import dispatcher
+from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext, run_async
+
+@run_async 
+def kemonomimi(update: Update, context: CallbackContext ):
+    bot = context.bot
+    msg = update.effective_message
+    user = update.effective_message.message_id
+    chat = update.effective_chat.id
+    nek = nekos.img("kemonomimi")
+    bot.animation (chat_id = chat, photo = nek, reply_to_message_id = user)
+   
+@run_async 
+def gasm(update: Update, context: CallbackContext ):
+    bot = context.bot
+    msg = update.effective_message
+    user = update.effective_message.message_id
+    chat = update.effective_chat.id
+    nek = nekos.img("gasm")
+    bot.animation (chat_id = chat, photo = nek, reply_to_message_id = user)
+
+@run_async 
+def poke(update: Update, context: CallbackContext ):
+    bot = context.bot
+    msg = update.effective_message
+    user = update.effective_message.message_id
+    chat = update.effective_chat.id
+    nek = nekos.img("poke")
+    bot.animation(chat_id = chat, animation= nek, reply_to_message_id = user)
+      
+@run_async 
+def anal(update: Update, context: CallbackContext ):
+    bot = context.bot
+    msg = update.effective_message
+    user = update.effective_message.message_id
+    chat = update.effective_chat.id
+    nek = nekos.img("anal")
+    bot.animation(chat_id = chat, animation = nek, reply_to_message_id = user)
+      
+@run_async 
+def slap(update: Update, context: CallbackContext ):
+    bot = context.bot
+    msg = update.effective_message
+    user = update.effective_message.message_id
+    chat = update.effective_chat.id
+    nek = nekos.img("slap")
+    bot.animation(chat_id = chat, animation = nek, reply_to_message_id = user)
+      
+@run_async 
+def hentai(update: Update, context: CallbackContext ):
+    bot = context.bot
+    msg = update.effective_message
+    user = update.effective_message.message_id
+    chat = update.effective_chat.id
+    nek = nekos.img("hentai")
+    bot.animation (chat_id = chat, photo = nek, reply_to_message_id = user)
+      
+@run_async 
+def ero(update: Update, context: CallbackContext ):
+    bot = context.bot
+    msg = update.effective_message
+    user = update.effective_message.message_id
+    chat = update.effective_chat.id
+    nek = nekos.img("ero")
+    bot.animation (chat_id = chat, photo = nek, reply_to_message_id = user)
+
+@run_async 
+def holo(update: Update, context: CallbackContext ):
+    bot = context.bot
+    msg = update.effective_message
+    user = update.effective_message.message_id
+    chat = update.effective_chat.id
+    nek = nekos.img("holo")
+    bot.animation (chat_id = chat, photo = nek, reply_to_message_id = user)
+
+@run_async 
+def blowjob(update: Update, context: CallbackContext ):
+    bot = context.bot
+    msg = update.effective_message
+    user = update.effective_message.message_id
+    chat = update.effective_chat.id
+    nek = nekos.img("blowjob")
+    bot.animation (chat_id = chat, photo = nek, reply_to_message_id = user)
+ 
+@run_async 
+def pussy(update: Update, context: CallbackContext ):
+    bot = context.bot
+    msg = update.effective_message
+    user = update.effective_message.message_id
+    chat = update.effective_chat.id
+    nek = nekos.img("pussy")
+    bot.animation(chat_id = chat, photo = nek, reply_to_message_id = user)
+
+@run_async 
+def boobs(update: Update, context: CallbackContext ):
+    bot = context.bot
+    msg = update.effective_message
+    user = update.effective_message.message_id
+    chat = update.effective_chat.id
+    nek = nekos.img("tits")
+    bot.animation(chat_id = chat, photo = nek, reply_to_message_id = user)
+
+run_async 
+def pussypic(update: Update, context: CallbackContext ):
+    bot = context.bot
+    msg = update.effective_message
+    user = update.effective_message.message_id
+    chat = update.effective_chat.id
+    nek = nekos.img("pussy_jpg")
+    bot.animation (chat_id = chat, photo = nek, reply_to_message_id = user)
+
+@run_async 
+def waifu(update: Update, context: CallbackContext ):
+    bot = context.bot
+    msg = update.effective_message
+    user = update.effective_message.message_id
+    chat = update.effective_chat.id
+    nek = nekos.img("waifu")
+    bot.animation (chat_id = chat, photo = nek, reply_to_message_id = user)
+
+
+@run_async 
+def kiss(update: Update, context: CallbackContext ):
+    bot = context.bot
+    msg = update.effective_message
+    user = update.effective_message.message_id
+    chat = update.effective_chat.id
+    nek = nekos.img("kiss")
+    bot.animation(chat_id = chat, animation = nek, reply_to_message_id = user)
+
+@run_async 
+def femdom(update: Update, context: CallbackContext ):
+    bot = context.bot
+    msg = update.effective_message
+    user = update.effective_message.message_id
+    chat = update.effective_chat.id
+    nek = nekos.img("femdom")
+    bot.animation(chat_id = chat, photo = nek, reply_to_message_id = user)
+
+@run_async 
+def spank(update: Update, context: CallbackContext ):
+    bot = context.bot
+    msg = update.effective_message
+    user = update.effective_message.message_id
+    chat = update.effective_chat.id
+    nek = nekos.img("spank")
+    bot.animation(chat_id = chat, photo = nek, reply_to_message_id = user)
+
+@run_async 
+def cuddle(update: Update, context: CallbackContext ):
+    bot = context.bot
+    msg = update.effective_message
+    user = update.effective_message.message_id
+    chat = update.effective_chat.id
+    nek = nekos.img("cuddle")
+    bot.animation(chat_id = chat, photo = nek, reply_to_message_id = user)
+
+@run_async 
+def erok(update: Update, context: CallbackContext ):
+    bot = context.bot
+    msg = update.effective_message
+    user = update.effective_message.message_id
+    chat = update.effective_chat.id
+    nek = nekos.img("erok")
+    bot.animation (chat_id = chat, photo = nek, reply_to_message_id = user)
+
+@run_async 
+def foxgirl(update: Update, context: CallbackContext ):
+    bot = context.bot
+    msg = update.effective_message
+    user = update.effective_message.message_id
+    chat = update.effective_chat.id
+    nek = nekos.img("fox_girl")
+    bot.photo(chat_id = chat, photo = nek, reply_to_message_id = user)
+
+@run_async 
+def smug(update: Update, context: CallbackContext ):
+    bot = context.bot
+    msg = update.effective_message
+    user = update.effective_message.message_id
+    chat = update.effective_chat.id
+    nek = nekos.img("smug")
+    bot.animation(chat_id = chat, animation = nek, reply_to_message_id = user)
+
+@run_async 
+def randomgif(update: Update, context: CallbackContext ):
+    bot = context.bot
+    msg = update.effective_message
+    user = update.effective_message.message_id
+    chat = update.effective_chat.id
+    nek = nekos.img("random_hentai_gif")
+    bot.send_photo(chat_id = chat, photo = nek, reply_to_message_id = user)
+
+@run_async 
+def baka(update: Update, context: CallbackContext ):
+    bot = context.bot
+    msg = update.effective_message
+    user = update.effective_message.message_id
+    chat = update.effective_chat.id
+    nek = nekos.img("baka")
+    bot.send_photo(chat_id = chat, photo = nek, reply_to_message_id = user)
+
+KEMO_HANDLER = DisableAbleCommandHandler("kemonomimi", kemonomimi)
+GASM_HANDLER = DisableAbleCommandHandler("gasm", gasm)
+POKE_HANDLER = DisableAbleCommandHandler("poke", poke)
+ANAL_HANDLER = DisableAbleCommandHandler("anal", anal) 
+SLAP_HANDLER = DisableAbleCommandHandler("slap", slap)
+HENTAI_HANDLER = DisableAbleCommandHandler("hentai", hentai )
+ERO_HANDLER = DisableAbleCommandHandler("ero", ero )
+HOLO_HANDLER = DisableAbleCommandHandler("holo", holo )
+BLOWJOB_HANDLER = DisableAbleCommandHandler("holo", holo )
+PUSSY_HANDLER = DisableAbleCommandHandler("pussy", pussy )
+BOOBS_HANDLER = DisableAbleCommandHandler("boobs", boobs )
+PUSSYPIC_HANDLER = DisableAbleCommandHandler("pussypic", pussyic )
+WAIFU_HANDLER = DisableAbleCommandHandler("waifu", waifu )
+KISS_HANDLER = DisableAbleCommandHandler("kiss", kiss )
+FEMDOM_HANDLER = DisableAbleCommandHandler("femdom", femdom )
+SPANK_HANDLER = DisableAbleCommandHandler("spank", spank )
+CUDDLE_HANDLER = DisableAbleCommandHandler("cuddle", cuddle )
+EROK_HANDLER = DisableAbleCommandHandler("erok", erok )
+FOXGIRL_HANDLER = DisableAbleCommandHandler("foxgirl", foxgirl )
+SMUG_HANDLER = DisableAbleCommandHandler("smug", smug )
+RANDOMGIF_HANDLER = DisableAbleCommandHandler("randomgif", randomgif )
+BAKA_HANDLER = DisableAbleCommandHandler("baka", baka )
+
+dispatcher.add_handler(KEMO_HANDLER)
+dispatcher.add_handler(GASM_HANDLER)
+dispatcher.add_handler(POKE_HANDLER)
+dispatcher.add_handler(ANAL_HANDLER)
+dispatcher.add_handler(SLAP_HANDLER)
+dispatcher.add_handler(HENTAI_HANDLER)
+dispatcher.add_handler(ERO_HANDLER)
+dispatcher.add_handler(HOLO_HANDLER)
+dispatcher.add_handler(BLOWJOB_HANDLER)
+dispatcher.add_handler(PUSSY_HANDLER)
+dispatcher.add_handler(BOOBS_HANDLER)
+dispatcher.add_handler(PUSSYPIC_HANDLER)
+dispatcher.add_handler(WAIFU_HANDLER)
+dispatcher.add_handler(KISS_HANDLER)
+dispatcher.add_handler(FEMDOM_HANDLER)
+dispatcher.add_handler(CUDDLE_HANDLER)
+dispatcher.add_handler(SPANK_HANDLER)
+dispatcher.add_handler(EROK_HANDLER)
+dispatcher.add_handler(BAKA_HANDLER)
+dispatcher.add_handler(FOXGIRL_HANDLER)
+dispatcher.add_handler(SMUG_HANDLER)
+
