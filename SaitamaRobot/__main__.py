8436cfe733c39fb01faf511fb8af75ce75ce1715
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-04-12 17:41:05 +0530

    update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -212,7 +212,7 @@ def start(update: Update, context: CallbackContext):
                      [   
                         InlineKeyboardButton(
                              text="Help! ", 
-                             callback_data="help_back" ) 
+                             callback_data="send_helpp" ) 
                        
                     ]]))
                   
@@ -257,7 +257,7 @@ def error_callback(update: Update, context: CallbackContext):
 @run_async
 def help_button(update, context):
     query = update.callback_query
-    help_match = re.match(r"send_helpp\((.+?)\)", query.data) 
+    help_match = re.match(r"send_helpp\((.+?)\)", query.data)
     mod_match = re.match(r"help_module\((.+?)\)", query.data)
     prev_match = re.match(r"help_prev\((.+?)\)", query.data)
     next_match = re.match(r"help_next\((.+?)\)", query.data)
@@ -302,12 +302,7 @@ def help_button(update, context):
                 reply_markup=InlineKeyboardMarkup(
                     paginate_modules(0, HELPABLE, "help")))
         elif help_match:
-            send_help(
-                          chat.id, text,
-                                      InlineKeyboardMarkup(
-                                                        [[InlineKeyboardButton(text="Back",
-                                                                                               callback_data="help_back")]]))
-                                                                                                
+            send_help(update.effective_chat.id, HELP_STRINGS) 
         # ensure no spinny white circle
         context.bot.answer_callback_query(query.id)
         # query.message.delete()
