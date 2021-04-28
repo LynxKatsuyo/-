97513a372a1145a6224aeaafe04d922af0447bf0
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-04-28 18:10:54 +0530

    Add files via upload

---
+++ SaitamaRobot/modules/Notify.py
@@ -0,0 +1,82 @@
+import html
+
+from SaitamaRobot import (LOGGER, DRAGONS, TIGERS, WOLVES, dispatcher)
+from SaitamaRobot.modules.helper_funcs.chat_status import (user_admin,
+                                                           user_not_admin)
+from SaitamaRobot.modules.log_channel import loggable
+from SaitamaRobot.modules.sql import reporting_sql as sql
+from telegram import (Chat, InlineKeyboardButton, InlineKeyboardMarkup,
+                      ParseMode, Update)
+from telegram.error import BadRequest, Unauthorized
+from telegram.ext import (CallbackContext, CallbackQueryHandler, CommandHandler,
+                          Filters, MessageHandler, run_async)
+from telegram.utils.helpers import mention_html
+
+REPORT_GROUP = 12
+REPORT_IMMUNE_USERS = DRAGONS + TIGERS + WOLVES
+
+@run_async 
+def req(update: Update, context: CallbackContext):
+  bot = context.bot 
+  args = context.args 
+  msg =update.effective_message
+  user = update.effective_user 
+  chat = update.effective_chat 
+  msg_id = update.effective_message.message_id 
+  
+  if chat and message.reply_to_message and sql.chat_should_report(chat.id):
+        requesting_user = user.id
+        chat_name = chat.title or chat.first or chat.username
+        admin_list = chat.get_administrators()
+        message = update.effective_message
+
+        if not args:
+            message.reply_text("Nyah boi request something!!")
+            return "" 
+        if chat.username and chat.type == Chat.SUPERGROUP:
+          requested = "Nyahh!! Request accepted!!" 
+          msg = f"<b>Request: </b>{html_escape(chat.title)}\n<b>Requested: </b> {args}\n<b>Requesting User:</b> {mention_html(user.id, user.first_name)}"
+          link = link = f'<b> • The message:</b> <a href="https://t.me/{chat.username}/{message.reply_to_message.message_id}">click here</a>'
+        for admin in admin_list:
+          if admin.user.is_bot():
+            #ignore bots
+            continue
+          if sql.user_should_report(admin.user.id):
+            try:
+              if not chat.type == Chat.SUPERGROUP:
+                        bot.send_message(
+                            admin.user.id,
+                            msg + link,
+                            parse_mode=ParseMode.HTML)
+                            
+              if not chat.username:
+                        bot.send_message(
+                            admin.user.id,
+                            msg + link,
+                            parse_mode=ParseMode.HTML)
+
+              if chat.username and chat.type == Chat.SUPERGROUP:
+                        bot.send_message(
+                            admin.user.id,
+                            msg + link,
+                            parse_mode=ParseMode.HTML)
+
+                except Unauthorized:
+                    pass
+                except BadRequest as excp:  # TODO: cleanup exceptions
+                    LOGGER.exception("Exception while reporting user")
+
+        message.reply_to_message.reply_text("Nyah! Request Accepted!!") 
+        return msg
+
+    return """"
+    doc
+    """
+  
+REQU_HANDLER = DisableAbleCommandHandler ("request", req) 
+REQ_HANDLER = DisableAbleMessageHandler(Filters.regex(r"^(?i)#request(.*)$"), afk, friendly="request")
+ 
+dispatcher.add_handler(REQU_HANDLER)
+dispatcher.add_handler(REQ_HANDLER)
+            
+          
