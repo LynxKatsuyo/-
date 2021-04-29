32e7baf047abf92f5fe53e81f5269606227ebbd1
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-04-29 13:17:44 +0530

    update Notify02.py

--- SaitamaRobot/modules/Notify02.py
+++ SaitamaRobot/modules/Notify02.py
@@ -59,7 +59,7 @@ def req(update: Update, context: CallbackContext):
           requested = "Nyahh!! Request accepted!!" 
           msg = f"<b>{html.escape(chat.title)}</b>\n<b>Requested: </b> {args}\n<b>Requesting User:</b> {mention_html(user.id, user.first_name)}"
           if chat.username:
-            link = link = f'<b> • The message:</b> <a href="https://t.me/{chat.username}/{message.message_id}">click here</a>'
+            link = f'\n<b> • The message:</b> <a href="https://t.me/{chat.username}/{message.message_id}">click here</a>'
           else:
             link = " "
         for admin in admin_list:
