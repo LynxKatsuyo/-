f3837e4897ebba563e6361b926ca0f09e9286399
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-05-01 21:45:48 +0530

    update welcome.py

--- SaitamaRobot/modules/welcome.py
+++ SaitamaRobot/modules/welcome.py
@@ -224,9 +224,9 @@ def new_member(update: Update, context: CallbackContext):
 
             # Welcome yourself
             elif new_mem.id == bot.id:
+                chat = update.effective_chat.id
                 creator = None
-                for x in bot.bot.get_chat_administrators(
-                        update.effective_chat.id):
+                for x in bot.chat.get_chat_administrators:
                     if x.status == 'creator':
                         creator = x.user
                         break
